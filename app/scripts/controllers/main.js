a3094b31117ae566202be62a1bf343447bfc5fad
Tomas Alabes <tomas.alabes@gmail.com>
2013-09-26 19:30:41 -0300

    to start angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,7 @@
 
 angular.module('QuickCompetitionApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+
+
   });
