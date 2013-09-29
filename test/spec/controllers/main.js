1dcabf4ed3908bba3796e7da9a1bafa4ad3703e6
Tomas Alabes <tomas.alabes@gmail.com>
2013-09-29 16:06:20 -0300

    1st test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,21 +2,23 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('QC'));
+    // load the controller's module
+    beforeEach(module('QC'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope, localStorageService) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope,
+            localStorageService: localStorageService
+        });
+    }));
+
+    it('should start with an empty contest', function () {
+        expect(scope.contest.participants.length).toBe(0);
+        expect(scope.contest.categories.length).toBe(0);
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
