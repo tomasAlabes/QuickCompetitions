1182a7886b7dfe0daf43c1d89bcbc303bf0615e2
Tomas Alabes <tomas.alabes@gmail.com>
2013-09-29 18:20:36 -0300

    Lint free

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -3,108 +3,116 @@
 var app = angular.module('QC');
 
 app.controller('MainCtrl', [
-        '$scope',
-        'localStorageService',
-        function ($scope, localStorageService) {
-            // Start fresh
-            var contest = $scope.contest = localStorageService.get('contest') || {
-                "participants": [],
-                "categories": []
-            };
+  '$scope',
+  'localStorageService',
+  function ($scope, localStorageService) {
 
-            $scope.categories = contest.categories;
-            $scope.participants = contest.participants;
-            $scope.categoryOptions = [{type:'1/5', "maxValue": 5}, {type:'1/10', "maxValue": 10}, {type:'1/100', "maxValue": 100}];
-            $scope.cType = $scope.categoryOptions[0];
+    function Contest() {
+      this.participants = [];
+      this.categories = [];
+    }
 
-            function randomId(){
-                return Math.floor(Math.random() * 10000000000);
-            }
+    function Participant(name) {
+      this.id = randomId();
+      this.name = name;
+      this.categories = [];
+    }
 
-            function Participant (name){
-                this.id = randomId();
-                this.name = name;
-                this.categories = [];
-            }
+    function Category(name, category) {
+      this.id = randomId();
+      this.name = name;
+      this.type = category.type;
+      this.maxValue = category.maxValue;
+      this.value = 1;
+    }
 
-            function Category (name, category){
-                this.id = randomId();
-                this.name = name;
-                this.type = category.type;
-                this.maxValue = category.maxValue;
-                this.value = 1;
-            }
+    function randomId() {
+      return Math.floor(Math.random() * 10000000000);
+    }
 
-            $scope.save = function() {
-                localStorageService.set('contest', contest);
-            };
+    // Start fresh
+    var contest = $scope.contest = localStorageService.get('contest') || new Contest();
 
-            $scope.addParticipant = function () {
-                var newParticipant = new Participant($scope.pName);
-                contest.participants.push(newParticipant);
+    $scope.categories = contest.categories;
+    $scope.participants = contest.participants;
+    $scope.categoryOptions = [
+      {type: '1/5', 'maxValue': 5},
+      {type: '1/10', 'maxValue': 10},
+      {type: '1/100', 'maxValue': 100}
+    ];
+    $scope.cType = $scope.categoryOptions[0];
 
-                for (var i = 0; i < contest.categories.length; i++) {
-                    newParticipant.categories.push(angular.copy(contest.categories[i]));
-                }
+    $scope.save = function () {
+      localStorageService.set('contest', contest);
+    };
 
-                $scope.pName = "";
-                $scope.save();
-            };
+    $scope.addParticipant = function () {
+      var newParticipant = new Participant($scope.pName);
+      contest.participants.push(newParticipant);
 
+      for (var i = 0; i < contest.categories.length; i++) {
+        newParticipant.categories.push(angular.copy(contest.categories[i]));
+      }
 
-            $scope.addCategory = function () {
-                var newCategory = new Category($scope.cName, $scope.cType);
-                contest.categories.push(newCategory);
-
-                for (var i = 0; i < contest.participants.length; i++) {
-                    contest.participants[i].categories.push(angular.copy(newCategory));
-                }
-
-                $scope.cName = "";
-                $scope.save();
-            };
+      $scope.pName = '';
+      $scope.save();
+    };
 
 
-            $scope.pKeyPressed = function(evt){
-                $scope.addParticipant();
-            };
+    $scope.addCategory = function () {
+      var newCategory = new Category($scope.cName, $scope.cType);
+      contest.categories.push(newCategory);
 
-            $scope.cKeyPressed = function(evt){
-                $scope.addCategory();
-            };
+      for (var i = 0; i < contest.participants.length; i++) {
+        contest.participants[i].categories.push(angular.copy(newCategory));
+      }
 
-            $scope.clearAll = function(){
-                localStorageService.clearAll();
-                $scope.contest.participants.length = 0;
-                $scope.contest.categories.length = 0;
-            }
+      $scope.cName = '';
+      $scope.save();
+    };
 
-            $scope.finish = function(){
-                var max = 0,
-                    winner;
 
-                for (var i = 0; i < contest.participants.length; i++) {
-                    var pValue = 0;
-                    for (var j = 0; j < contest.participants[i].categories.length; j++) {
-                        pValue += contest.participants[i].categories[j].value;
-                    }
-                    if(pValue > max){
-                        max = pValue;
-                        winner = contest.participants[i];
-                    }
-                    pValue = 0;
-                }
+    $scope.pKeyPressed = function () {
+      $scope.addParticipant();
+    };
 
-                $scope.showWinner = true;
-                $scope.winnerMsg = "Congratulations " + winner.name + ", you won with " + max + " points!!!!";
+    $scope.cKeyPressed = function () {
+      $scope.addCategory();
+    };
 
-            }
+    $scope.clearAll = function () {
+      localStorageService.clearAll();
+      $scope.contest.participants.length = 0;
+      $scope.contest.categories.length = 0;
+    };
 
-            $scope.$watch('participants + categories', function(){
-                $scope.disableFinish = $scope.participants.length === 0;
-                $scope.disableClearAll = $scope.participants.length === 0 && $scope.categories.length === 0;
-            });
+    $scope.finish = function () {
+      var max = 0,
+        winner;
 
-            $scope.showWinner = false;
+      for (var i = 0; i < contest.participants.length; i++) {
+        var pValue = 0;
+        for (var j = 0; j < contest.participants[i].categories.length; j++) {
+          pValue += contest.participants[i].categories[j].value;
+        }
+        if (pValue > max) {
+          max = pValue;
+          winner = contest.participants[i];
+        }
+        pValue = 0;
+      }
 
-        }]);
+      $scope.showWinner = true;
+      $scope.winnerMsg = 'Congratulations ' + winner.name + ', you won with ' + max + ' points!!!!';
+
+    };
+
+    $scope.$watch('participants + categories', function () {
+      $scope.disableFinish = $scope.participants.length === 0;
+      $scope.disableClearAll = $scope.participants.length === 0 && $scope.categories.length === 0;
+    });
+
+    $scope.showWinner = false;
+
+  }
+]);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,23 +2,23 @@
 
 describe('Controller: MainCtrl', function () {
 
-    // load the controller's module
-    beforeEach(module('QC'));
+// load the controller's module
+  beforeEach(module('QC'));
 
-    var MainCtrl,
-        scope;
+  var MainCtrl,
+    scope;
 
-    // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller, $rootScope, localStorageService) {
-        scope = $rootScope.$new();
-        MainCtrl = $controller('MainCtrl', {
-            $scope: scope,
-            localStorageService: localStorageService
-        });
-    }));
-
-    it('should start with an empty contest', function () {
-        expect(scope.contest.participants.length).toBe(0);
-        expect(scope.contest.categories.length).toBe(0);
+// Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope, localStorageService) {
+    scope = $rootScope.$new();
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope,
+      localStorageService: localStorageService
     });
+  }));
+
+  it('should start with an empty contest', function () {
+    expect(scope.contest.participants.length).toBe(0);
+    expect(scope.contest.categories.length).toBe(0);
+  });
 });
