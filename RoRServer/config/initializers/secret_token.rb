133825d2076f74cff6d8cc5b49b65ef436183976
Tomas Alabes <tomas.alabes@gmail.com>
2013-10-18 23:36:09 -0300

    RoRServer v0

---
+++ RoRServer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoRServer::Application.config.secret_key_base = 'cc1fc5e5865e74da8b4fd6cbc489171d73996391297790445385805bd953145ca1f78199889b626c34c1a4971a35ce953f02c5bf584677a38d089aab32688bb9'
---
+++ RoRServer/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoRServer::Application.config.session_store :cookie_store, key: '_RoRServer_session'
